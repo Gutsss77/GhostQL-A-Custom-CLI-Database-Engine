7a21cf5bc532f74555460ea5a804c26a24b860cc
Ansh sharma <ansh11639@gmail.com>
2025-06-05 14:03:13 +0530

    added QLParser.h for creating QLParser.cpp uses class trick

--- include/QLParser.h
+++ include/QLParser.h
@@ -6,12 +6,11 @@
 
 class QLParser {
 private:
-    std::string toUpperCase(const std::string& command);
     std::vector<std::string> tokensOfCommand(const std::string& command);
 
 public:
+    std::string toUpperCase(const std::string& command);
     std::vector<std::string> tokenizer(const std::string& s);
-    std::string getUpper(const std::string& command);
 };
 
 #endif // QLPARSER_H
