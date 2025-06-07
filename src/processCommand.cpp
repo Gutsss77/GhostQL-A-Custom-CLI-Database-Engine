837d694e247dc6548edacaf4eb909b62606ccd39
Ansh sharma <ansh11639@gmail.com>
2025-06-07 20:04:13 +0530

    exit function implemented and parse for create database command complete

--- src/processCommand.cpp
+++ src/processCommand.cpp
@@ -1,6 +1,6 @@
-#include "processCommand.h"
-#include "database.h"
-#include "QLParser.h"
+#include "processCommand.hpp"
+#include "database.hpp"
+#include "QLParser.hpp"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -8,15 +8,31 @@
 Database db;
 QLParser qlp;
 
+//for exiting the program;
+void exitProgram() {
+    std::cout << "Exiting GhostQL. Goodbye!\n";
+    exit(0);
+}
+
+//function to process input commands
 void processCommand(std::vector<std::string>& tokens){
-    if(tokens.size() < 3){
-        std::cerr << "Invalid Command!";
+    if (tokens.empty()) {
+        std::cerr << "Empty command.\n";
         return;
     }
     std::string commandType = tokens[0];
+    if(commandType == "EXIT"){
+        exitProgram();
+    }
+    std::string commandFunction = tokens[1];
 
     if(commandType == "CREATE"){
-        db.createDatabase(tokens);
-        return;
+        if(commandFunction == "DATABASE"){
+            db.createDatabase(tokens);
+            return;
+        }else if(commandFunction == "TABLE"){
+            
+        }
     }
+    std::cerr << "Unsupported or unknown command.\n";
 }
