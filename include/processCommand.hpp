001b3530ac6ef5eeb38a8a08fe1ec7e2f1548775
Ansh sharma <ansh11639@gmail.com>
2025-06-07 20:02:18 +0530

    added incomplete processCoammnd function and complete exit function

---
+++ include/processCommand.hpp
@@ -0,0 +1,10 @@
+#ifndef PROCESS_COMMAND_H
+#define PROCESS_COMMAND_H
+
+#include <vector>
+#include <string>
+
+void processCommand(std::vector<std::string>& tokens);
+void exitProgram();
+
+#endif //PROCESS_COMMAND_H
