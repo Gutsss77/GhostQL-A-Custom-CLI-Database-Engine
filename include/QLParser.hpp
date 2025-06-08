7aac2a26f63145728035507c343bacf944b15ff5
Ansh sharma <ansh11639@gmail.com>
2025-06-08 11:02:51 +0530

    updated QLParer for handling commands

---
+++ include/QLParser.hpp
@@ -0,0 +1,23 @@
+#ifndef QLPARSER_H
+#define QLPARSER_H
+
+#include <string>
+#include <vector>
+
+class QLParser {
+private:
+    //covert the string into vector of tokens
+    std::vector<std::string> tokensOfCommand(const std::string& command);
+
+public:
+    //convert the command to uppercase
+    std::string toUpperCase(const std::string& command);
+
+    //return vector containing all uppercase tokens
+    std::vector<std::string> tokenizer(const std::string& s);
+
+    //creates meta data folder in users table for defining schemas
+    void extractMetadataForTable(std::vector<std::string>& tokens);
+};
+
+#endif // QLPARSER_H
