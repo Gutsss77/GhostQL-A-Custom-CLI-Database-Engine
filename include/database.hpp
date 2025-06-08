e743c17dd49529995cb365f8f1b29df9d1f99c0b
Ansh sharma <ansh11639@gmail.com>
2025-06-08 11:04:05 +0530

    handling database functions

---
+++ include/database.hpp
@@ -0,0 +1,17 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <string>
+#include "session.hpp"
+#include <vector>
+
+class Database{
+public:
+    void createDatabase(SessionContext& ctx, std::vector<std::string>& tokens);
+    void useDatabase(SessionContext& ctx, std::vector<std::string>& tokens);
+    void createTable(SessionContext& ctx, std::vector<std::string>& tokens);
+};
+
+
+
+#endif //DATABASE_H
