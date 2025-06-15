181d85b26ff0f44c40c739e1f188bd2e727a4fcc
Ansh Sharma <ansh11639@gmail.com>
2025-06-15 17:14:25 +0530

    parser for drop database command

--- src/processCommand.cpp
+++ src/processCommand.cpp
@@ -18,8 +18,8 @@ void exitProgram() {
 
 // Parses and tokenizes input string and routes to command processor
 void process(SessionContext& ctx, const std::string& input) {
-    std::string sql = qlp.toUpperCase(input);
-    std::vector<std::string> tokens = qlp.tokenizer(sql);
+    // std::string sql = qlp.toUpperCase(input);
+    std::vector<std::string> tokens = qlp.tokenizer(input);
     processCommand(ctx, tokens);
 }
 
@@ -30,7 +30,8 @@ void processCommand(SessionContext& ctx, std::vector<std::string>& tokens) {
         return;
     }
 
-    std::string commandType = tokens[0];
+    // std::string commandType = tokens[0];
+    std::string commandType = qlp.toUpperCase(tokens[0]);
 
     if (commandType == "EXIT") {
         exitProgram();
@@ -52,7 +53,8 @@ void processCommand(SessionContext& ctx, std::vector<std::string>& tokens) {
             return;
         }
 
-        std::string commandFunction = tokens[1];
+        // std::string commandFunction = tokens[1];
+        std::string commandFunction = qlp.toUpperCase(tokens[1]);
 
         if (commandFunction == "DATABASE") {
             db.createDatabase(ctx, tokens);
@@ -71,13 +73,30 @@ void processCommand(SessionContext& ctx, std::vector<std::string>& tokens) {
             std::cerr << " ERROR : Invalid SHOW command. Usage: SHOW DATABASES | SHOW TABLES\n";
             return;
         }
-        std::string commandFunction = tokens[1];
+        // std::string commandFunction = tokens[1];
+        std::string commandFunction = qlp.toUpperCase(tokens[1]);
         if(commandFunction == "DATABASES"){
             db.showDatabases(ctx, tokens);
             return;
         }else if(commandFunction == "TABLES"){
             db.showTables(ctx, tokens);
             return;
+        }else{
+            std::cerr << " ERROR : Invalid command.\n";
+            return;
+        }
+    }
+
+    if(commandType == "DROP"){
+        if(tokens.size() > 3){
+            std::cerr << " ERROR : Invalid DROP command. Usage : DROP <databasename> | DROP TABLE <tablename>\n";
+            return;
+        }
+        std::string commandFunction = qlp.toUpperCase(tokens[1]);
+        if(commandFunction == "TABLE"){
+            return;
+        }else if(tokens.size() == 2){
+            db.dropDatabase(ctx, tokens);
         }else{
             std::cerr << " ERROR : Invalid command.\n";
             return;
