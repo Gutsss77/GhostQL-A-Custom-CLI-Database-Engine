956b43cc03f1e6c38f7e856bc4be75a0a6fdbef5
Ansh Sharma <ansh11639@gmail.com>
2025-06-22 09:48:05 +0530

    added a some helpers (jsonColumnTokens(), columnNamesFromQuery(), valuesFromQuery()

--- src/helper.cpp
+++ src/helper.cpp
@@ -1,23 +1,82 @@
-#include <helper.hpp>
+#include "helper.hpp"
+#include "QLParser.hpp"
 #include <json.hpp>
-#include <QLParser.hpp>
 #include <filesystem>
 #include <iostream>
-#include <vector>
-#include <string>
 
 using json = nlohmann::ordered_json;
 namespace fs = std::filesystem;
+QLParser qlp;
 
-
-std::vector<std::string> Helper::jsonColumnTokens(json schema, fs::path table){
+std::vector<std::string> Helper::jsonColumnTokens(json schema, fs::path table) {
     std::vector<std::string> newTokens;
-    json j = json::parse(schema);
-
-    for(const auto& i : j["metaData"]){
-        if(i.contains("column_name")){
+    for (const auto& i : schema["metaData"]) {
+        if (i.contains("column_name")) {
             newTokens.push_back(i["column_name"]);
         }
     }
     return newTokens;
+}
+
+std::vector<std::string> Helper::columnNamesFromQuery(std::vector<std::string>& tokens) {
+    int idxF = -1;
+    int idxL = -1;
+    int n = tokens.size();
+    for (int i = 0; i < n; i++) {
+        if (tokens[i] == "(") {
+            idxF = i;
+        }
+        if (tokens[i] == ")") {
+            idxL = i;
+            break;
+        }
+    }
+
+    std::vector<std::string> newTokens;
+    for (int i = idxF + 1; i < idxL; i++) {
+        if (tokens[i] != ",") {
+            newTokens.push_back(tokens[i]);
+        }
+    }
+    return newTokens;
+}
+
+std::vector<std::vector<std::string>> Helper::valuesFromQuery(std::vector<std::string>& tokens){
+    std::vector<std::vector<std::string>> valuesSet;
+    int n = tokens.size();
+    int i = 0;
+    while(i < n && qlp.toUpperCase(tokens[i]) != "VALUES"){
+        i++;
+    }
+
+    if(i == n){
+        std::cerr << " ERROR : 'VALUES' keyword not found.\n";
+        return valuesSet;
+    }
+
+    i++; //moves to next token after VALUES
+    while(i < n){
+        if(tokens[i] == "("){
+            std::vector<std::string> rowData;
+            i++; // move after the '('
+            while(i < n && tokens[i] != ")"){
+                if(tokens[i] != ","){
+                    std::string val = tokens[i];
+                        //removes quotes from strings like ('Ghost') to (Ghost)
+                    if(!val.empty() && val.front() == '\'' && val.back() == '\''){
+                        val = val.substr(1, val.size() - 2);
+                    }
+                    rowData.push_back(val);
+                }
+                i++;
+            }
+            if(i == n || tokens[i] != ")"){
+                std::cerr << " ERROR: Missing closing ')' in VALUES clause.\n";
+                return valuesSet;
+            }
+            valuesSet.push_back(rowData);
+        }
+        i++;
+    }
+    return valuesSet;
 }
